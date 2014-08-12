8f008246abe6c906ee19781d2cb158223523e044
Chadwick Hodges <chadwickh@gmail.com>
2014-08-11 21:11:27 -0600

    Refactored

--- lager-lite.js
+++ lager-lite.js
@@ -15,6 +15,8 @@ if (Meteor.isClient) {
     var labels=['Time']
     var data=[]
     var times = []
+    console.log('In graphit function')
+    console.log(metric)
 
     // There's gotta be a better way - but I haven't figured it out yet
     // Basically had an issue using the milliseconds from epoch as an arry index and had to change to
@@ -23,10 +25,12 @@ if (Meteor.isClient) {
         times.push(time)
     }
 
-    times.sort();
+    times.sort()
+    console.log(times)
 
     for (var key in metric['labels']) {
        labels.push(key)
+       console.log(key)
        for (i=0; i < times.length; i++) {
           time=times[i]
           if (data[i] === undefined) {
@@ -36,6 +40,8 @@ if (Meteor.isClient) {
           data[i].push(metric['data'][time][key])
        }
     }
+
+    console.log(data)
 
     LAGER[div]['graph'] = new Dygraph(document.getElementById(graph_div),
                            data,
@@ -115,6 +121,7 @@ if (Meteor.isClient) {
         LAGER[LAGER.metrics[i]]['data']={}
         LAGER[LAGER.metrics[i]]['graph']={}
         LAGER[LAGER.metrics[i]]['max']={}
+        LAGER[LAGER.metrics[i]]['sum']={}
         LAGER[LAGER.metrics[i]]['average']={}
       }
       Session.set("filesCount",0)
@@ -131,6 +138,7 @@ if (Meteor.isClient) {
     'change input':  function() {
       var filecount = 0
       var fileList = []
+      var data_fields = {}
       fileList = document.getElementById('input').files
       Session.set("filesCount",fileList.length)
 
@@ -172,230 +180,89 @@ if (Meteor.isClient) {
                console.log('Processing array:  '+serial+'  from:  '+start+' to:  '+end)
              } else if (proc_re.test(line)) {
                metric='CPU'
-               //console.log('In CPU section')
+               // series_fields is an array of fields to concatenate with dashes to build the 
+               // series name
+               series_fields=[0,1]
+               // data_fields is a hash with the key being the metric name and the value
+               // the field from the line to use
+               data_fields[metric]=[2]
+               console.log('In CPU section')
                return true
              } else if (cwp_re.test(line)) {
                metric='CWP'
-               //console.log('In CWP section')
+               series_fields=[0,1]
+               data_fields[metric]=[2]
+               console.log('In CWP section')
                return true
              } else if (port_re.test(line)) {
                metric='Port'
-               //console.log('In Port section')
+               series_fields=[0,1]
+               data_fields[metric+'_IOPS']=[2]
+               data_fields[metric+'_MBPS']=[7]
+               console.log('In Port section')
                return true
              } else if (dp_re.test(line)) {
                metric='DP'
-               //console.log('In DP section')
+               series_fields=[0,1]
+               data_fields[metric+'_IOPS']=[2]
+               data_fields[metric+'_MBPS']=[7]
+               console.log('In DP section')
                return true
              } else if (lun_re.test(line)) {
                metric='LUN'
-               //console.log('In LUN section')
+               series_fields=[0,1]
+               data_fields[metric+'_IOPS']=[2]
+               data_fields[metric+'_MBPS']=[7]
+               console.log('In LUN section')
                return true
              } else if (tags_re.test(line)) {
                metric='LUN_TAGS'
-               //console.log('In LUN_TAGS section')
+               series_fields=[0,1]
+               data_fields[metric]=[2]
+               console.log('In LUN_TAGS section')
                return true
              } else if (drive_re.test(line)) {
                metric='DISK_BUSY'
-               //console.log('In DISK_BUSY section')
+               series_fields=[0,1,2]
+               data_fields[metric]=[3]
+               console.log('In DISK_BUSY section')
                return true
              }
              switch (metric) {
                case 'CPU':
-                 //console.log(line)
-                 proc_line=line.split(/\s+/)
-                 proc=proc_line[0]+'-'+proc_line[1]
-                 usage=parseFloat(proc_line[2])
-                 LAGER[metric]['labels'][proc]=1
-                 if (LAGER[metric]['data'][start] === undefined) {
-                     LAGER[metric]['data'][start] = {}
-                 }
-                 LAGER[metric]['data'][start][proc]=usage
-                 // Set per processor maximum
-                 if (LAGER[metric]['max'][proc] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[metric]['max'][proc] = usage
-                 } else if (usage > LAGER[metric]['max'][proc]) {
-                     LAGER[metric]['max'][proc] = usage
-                 }
-                 break
                case 'CWP':
-                 cwp_line=line.split(/\s+/)
-                 clpr=cwp_line[0]+'-'+cwp_line[1]
-                 //console.log('CLPR:  '+clpr)
-                 cwp=parseFloat(cwp_line[2])
-                 LAGER[metric]['labels'][clpr]=1
-                 if (LAGER[metric]['data'][start] === undefined) {
-                     LAGER[metric]['data'][start] = {}
-                 }
-                 LAGER[metric]['data'][start][clpr]=cwp
-                 // Set per clpr maximum
-                 if (LAGER[metric]['max'][clpr] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[metric]['max'][clpr] = cwp
-                 } else if (usage > LAGER[metric]['max'][clpr]) {
-                     LAGER[metric]['max'][clpr] = cwp
-                 }
-                 break
                case 'Port':
-                 iops_metric='Port_IOPS'
-                 mbps_metric='Port_MBPS'
-                 //console.log(line)
-                 port_line=line.split(/\s+/)
-                 //console.log(port_line)
-                 port=port_line[0]+'-'+port_line[1]
-                 iops=parseFloat(port_line[2])
-                 mbps=parseFloat(port_line[7])
-                 // Per-Port IOPS
-                 LAGER[iops_metric]['labels'][port]=1
-                 if (LAGER[iops_metric]['data'][start] === undefined) {
-                     LAGER[iops_metric]['data'][start] = {}
-                 }
-                 LAGER[iops_metric]['data'][start][port]=iops
-                 // Set per-port iops maximum
-                 if (LAGER[iops_metric]['max'][port] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[iops_metric]['max'][port] = iops
-                 } else if (iops > LAGER[iops_metric]['max'][port]) {
-                     LAGER[iops_metric]['max'][port] = iops
-                 }
-                 // Per-Port MBPS
-                 LAGER[mbps_metric]['labels'][port]=1
-                 if (LAGER[mbps_metric]['data'][start] === undefined) {
-                     LAGER[mbps_metric]['data'][start] = {}
-                 }
-                 LAGER[mbps_metric]['data'][start][port]=mbps
-                 // Set per-port mbps maximum
-                 if (LAGER[mbps_metric]['max'][port] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[mbps_metric]['max'][port] = mbps
-                 } else if (mbps > LAGER[mbps_metric]['max'][port]) {
-                     LAGER[mbps_metric]['max'][port] = mbps
-                 }
-                 break
                case 'DP':
-                 iops_metric='DP_IOPS'
-                 mbps_metric='DP_MBPS'
-                 //console.log(line)
-                 dp_line=line.split(/\s+/)
-                 //console.log(dp_line)
-                 pool=dp_line[0]+'-'+dp_line[1]
-                 iops=parseFloat(dp_line[2])
-                 mbps=parseFloat(dp_line[7])
-                 // Per-DP Pool IOPS
-                 LAGER[iops_metric]['labels'][pool]=1
-                 if (LAGER[iops_metric]['data'][start] === undefined) {
-                     LAGER[iops_metric]['data'][start] = {}
-                 }
-                 LAGER[iops_metric]['data'][start][pool]=iops
-                 // Set per-DP Pool mbps maximum
-                 if (LAGER[iops_metric]['max'][pool] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[iops_metric]['max'][pool] = iops
-                 } else if (iops > LAGER[iops_metric]['max'][pool]) {
-                     LAGER[iops_metric]['max'][pool] = iops
-                 }
-                 // Per-DP Pool MBPS
-                 LAGER[mbps_metric]['labels'][pool]=1
-                 if (LAGER[mbps_metric]['data'][start] === undefined) {
-                     LAGER[mbps_metric]['data'][start] = {}
-                 }
-                 LAGER[mbps_metric]['data'][start][pool]=mbps
-                 // Set per-DP Pool mbps maximum
-                 if (LAGER[mbps_metric]['max'][pool] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[mbps_metric]['max'][pool] = mbps
-                 } else if (mbps > LAGER[mbps_metric]['max'][pool]) {
-                     LAGER[mbps_metric]['max'][pool] = mbps
-                 }
-                 //console.log(pool, iops, mbps)
-                 break
                case 'LUN':
-                 iops_metric='LUN_IOPS'
-                 mbps_metric='LUN_MBPS'
-                 block_size_metric='BLOCK_SIZE'
-                 //console.log(line)
-                 lun_line=line.split(/\s+/)
-                 //console.log(lun_line)
-                 lun=lun_line[0]+'-'+lun_line[1]
-                 iops=parseFloat(lun_line[2])
-                 mbps=parseFloat(lun_line[7])
-                 block_size=mbps*1024/iops
-                 LAGER[iops_metric]['labels'][lun]=1
-                 if (LAGER[iops_metric]['data'][start] === undefined) {
-                     LAGER[iops_metric]['data'][start] = {}
-                 }
-                 LAGER[iops_metric]['data'][start][lun]=iops
-                 // Set per-LUN iops maximum
-                 if (LAGER[iops_metric]['max'][lun] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[iops_metric]['max'][lun] = iops
-                 } else if (iops > LAGER[iops_metric]['max'][lun]) {
-                     LAGER[iops_metric]['max'][lun] = iops
-                 }
-                 //
-                 LAGER[mbps_metric]['labels'][lun]=1
-                 if (LAGER[mbps_metric]['data'][start] === undefined) {
-                     LAGER[mbps_metric]['data'][start] = {}
-                 }
-                 LAGER[mbps_metric]['data'][start][lun]=mbps
-                 // Set per-LUN mbps maximum
-                 if (LAGER[mbps_metric]['max'][lun] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[mbps_metric]['max'][lun] = mbps
-                 } else if (mbps > LAGER[mbps_metric]['max'][lun]) {
-                     LAGER[mbps_metric]['max'][lun] = mbps
-                 }
-                 //
-                 LAGER[block_size_metric]['labels'][lun]=1
-                 if (LAGER[block_size_metric]['data'][start] === undefined) {
-                     LAGER[block_size_metric]['data'][start] = {}
-                 }
-                 LAGER[block_size_metric]['data'][start][lun]=block_size
-                 // Set per-LUN block size maximum
-                 if (LAGER[block_size_metric]['max'][lun] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[block_size_metric]['max'][lun] = block_size
-                 } else if (mbps > LAGER[block_size_metric]['max'][lun]) {
-                     LAGER[block_size_metric]['max'][lun] = block_size
-                 }
-                 //console.log(lun, iops, mbps)
-                 break
                case 'LUN_TAGS':
-                 tag_line=line.split(/\s+/)
-                 lun=tag_line[0]+'-'+tag_line[1]
-                 tags=parseFloat(tag_line[2])
-                 LAGER[metric]['labels'][lun]=1
-                 if (LAGER[metric]['data'][start] === undefined) {
-                     LAGER[metric]['data'][start] = {}
-                 }
-                 LAGER[metric]['data'][start][lun]=tags
-                 // Set per-LUN tag maximum
-                 if (LAGER[metric]['max'][lun] === undefined) {
-                     //console.log("In undefined section for Tags for lun" + lun + " with tags: " + tags)
-                     LAGER[metric]['max'][lun] = tags
-                 } else if (tags > LAGER[metric]['max'][lun]) {
-                     //console.log("Setting max tags to: " + tags + " for LUN:  "+lun)
-                     LAGER[metric]['max'][lun] = tags
-                 }
-                 break
                case 'DISK_BUSY':
+                 //console.log(metric)
                  //console.log(line)
-                 drive_line=line.split(/\s+/)
-                 //console.log(drive_line)
-                 drive=drive_line[0]+'-'+drive_line[1]+'-'+drive_line[2]
-                 busy=parseFloat(drive_line[3])
-                 //console.log(drive, busy)
-                 LAGER[metric]['labels'][drive]=1
-                 if (LAGER[metric]['data'][start] === undefined) {
-                     LAGER[metric]['data'][start] = {}
+                 temp_line=line.split(/\s+/)
+                 series=temp_line[series_fields[0]]
+                 //console.log("Setting series to:  " + series)
+                 if (series_fields.length > 0) {
+                    for (i=1; i < series_fields.length; i++) {
+                       series=series+"-"+temp_line[series_fields[i]]
+                       //console.log("Setting series to:  " + series)
+                    }
                  }
-                 LAGER[metric]['data'][start][drive]=busy
-                 // Set per-LUN tag maximum
-                 if (LAGER[metric]['max'][drive] === undefined) {
-                     //console.log("In undefined section for max")
-                     LAGER[metric]['max'][drive] = busy
-                 } else if (mbps > LAGER[metric]['max'][drive]) {
-                     LAGER[metric]['max'][drive] = busy
+                 for (var key in data_fields) {
+                    // check to make sure that the metric and key are related - 
+                    // otherwise you'll enumerate over every key for every series 
+                    if (key.indexOf(metric) > -1) {
+                       if (data_fields.hasOwnProperty(key)) {
+                          //console.log("Got key: " + key + " with value: " + data_fields[key])
+                          data=parseFloat(temp_line[data_fields[key]])
+                          //console.log("Got value: " + data + " for data")
+                          LAGER[key]['labels'][series]=1
+                          if (LAGER[key]['data'][start] === undefined) {
+                              LAGER[key]['data'][start] = {}
+                          }
+                          //console.log('Key: ' + key +', series: '+ series + ', data: ' + data)
+                          LAGER[key]['data'][start][series]=data
+                       }
+                    }
                  }
                  break
              }
@@ -405,8 +272,21 @@ if (Meteor.isClient) {
         reader.onloadend = function(e) {
            filecount++
            Session.set("filesLoaded",filecount)
-           //console.log('Filecount:  '+filecount+'  fileList.length:  '+fileList.length)
+           console.log('Filecount:  '+filecount+'  fileList.length:  '+fileList.length)
            if (filecount >= fileList.length) {
+              for (var time in LAGER['LUN_IOPS']['data']) {
+                 if (LAGER['LUN_IOPS']['data'].hasOwnProperty(time)) {
+                    for (var lun in LAGER['LUN_IOPS']['data'][time]) {
+                       if (LAGER['LUN_IOPS']['data'][time].hasOwnProperty(lun)) {
+                          LAGER['BLOCK_SIZE']['labels'][lun]=1
+                          if (LAGER['BLOCK_SIZE']['data'][time] === undefined) {
+                             LAGER['BLOCK_SIZE']['data'][time] = {}
+                          }
+                          LAGER['BLOCK_SIZE']['data'][time][lun]=(LAGER['LUN_MBPS']['data'][time][lun]*1024)/LAGER['LUN_IOPS']['data'][time][lun]
+                       } 
+                    }
+                 }
+              }
               console.log('LAGER data structure')
               console.log(LAGER)
               for (var i=0; i <  LAGER.metrics.length; i++) {
